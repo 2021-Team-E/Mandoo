b666a3f7904b3d26efd4392f8a11cc0eccdf2938
Gnu-Kenny <qkr0677@gmail.com>
2021-07-05 21:34:58 +0900

    feat: mongo DB 테스트 url 추가

--- Backend/app.py
+++ Backend/app.py
@@ -1,7 +1,33 @@
-from flask import Flask
+from flask import Flask, jsonify
+from pymongo import MongoClient
+from bson.json_util import dumps
+from bson import json_util
+
+
 app = Flask(__name__)
-@app.route('/')
+
+mongo = MongoClient('mongo_db', 27017)
+
+
+@app.route('/', methods=['GET'])
 def home():
     return 'Hello, World!'
-if __name__ == '__main__':
-    app.run(debug=True)
+
+
+@app.route('/mongo', methods=['GET'])
+def mongo_fetch():
+    db = mongo.netflix
+    netflix = db.netflix_titles.find()
+    result = dumps(netflix, default=json_util.default)
+    return jsonify(result=result)
+
+
+@app.route('/mongo/kpop', methods=['GET'])
+def mongo_kpop_fetch():
+    db = mongo.kpop
+    girl_grops = db.kpop_idols_girl_groups.find()
+    result = dumps(girl_grops, default=json_util.default)
+    return jsonify(result=result)
+
+
+app.run(host='0.0.0.0',debug=True)
