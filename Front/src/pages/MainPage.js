72e3e6bd108c1d44c01828f95d7c21a0ca596f67
leecr1215 <leecr1215@naver.com>
2021-07-07 16:50:09 +0900

    feat: 이미지 서버로 전송

--- Front/src/pages/MainPage.js
+++ Front/src/pages/MainPage.js
@@ -3,6 +3,7 @@ import BlankTop from '../components/BlankTop';
 import {useHistory} from 'react-router-dom';
 import {useState} from 'react';
 import axios from 'axios';
+import { USER_SERVER } from '../config';
 
 const border = {
     marginTop: "40px",
@@ -12,27 +13,48 @@ const border = {
     height: "200px"
 };
 
-const MainPage = () => {
+
+const MainPage = (props) => {
     
     const history = useHistory();
     const [fileUrl, setFileUrl] = useState(null);
+    const [fileImg, setFileImg] = useState(null);
 
+    // 이미지 업로드 버튼 이벤트. 미리보기 
     function processImage(event) {
         const imageFile = event.target.files[0];
         const imageUrl = URL.createObjectURL(imageFile);
+        
         setFileUrl(imageUrl)
+        setFileImg(imageFile)
     }
 
+    // 전송 버튼 클릭 이벤트
+    const sendImage = (event) => {
+        try {
+            const request = axios.post(`${USER_SERVER}/getimg`, {fileImg})
+            .then(function (response) {console.log(response);})
+            return {
+                payload: request
+            }
+        }
+        catch(e){
+            console.log("error");
+        }
+    
+    }
  
     return(
         <div>
-            
+            <div className="nav">
+                
+            </div>
             <BlankTop DesktopMargin='3' TabletMargin='3' MobileMargin='1'/>
             <button onClick={() => history.push((`/`))}>메인페이지</button>
             <button onClick={() => history.push((`/signup`))}>회원가입</button>
             <button onClick={() => history.push((`/login`))}>로그인</button>
             <div style={border}>
-                 <img style={{"object-fit": "fill", "width": "150px", "height": "200px"}} src={fileUrl} alt={fileUrl} />
+                 <img style={{"objectFit": "fill", "width": "150px", "height": "200px"}} src={fileUrl} alt={fileUrl} />
             </div>
             <div>
                 <input type='file'
@@ -40,8 +62,9 @@ const MainPage = () => {
                     name='question_img'
                     onChange={processImage}>
                 </input>
-                <button onClick={null}>전송</button>
+                <button onClick={sendImage}>전송</button>
             </div>
+            
         </div>
     );
 }
