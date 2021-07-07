ecaaac5a86391be8ef0e826dda656a1b23db141d
leecr1215 <leecr1215@naver.com>
2021-07-07 18:14:54 +0900

    feat: 네이게이션 바 html

--- Front/src/pages/MainPage.js
+++ Front/src/pages/MainPage.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import BlankTop from '../components/BlankTop';
-import {useHistory} from 'react-router-dom';
+//import BlankTop from '../components/BlankTop';
+//import {useHistory} from 'react-router-dom';
 import {useState} from 'react';
 import axios from 'axios';
 import { USER_SERVER } from '../config';
+import logo from './logo.PNG';
+import { Link } from 'react-router-dom';
 
-const border = {
+// 테두리 만드는 css
+const divBorder = {
     marginTop: "40px",
     marginBottom: "40px",
     border: "1px solid black",
@@ -16,7 +19,7 @@ const border = {
 
 const MainPage = (props) => {
     
-    const history = useHistory();
+    //const history = useHistory();
     const [fileUrl, setFileUrl] = useState(null);
     const [fileImg, setFileImg] = useState(null);
 
@@ -30,41 +33,44 @@ const MainPage = (props) => {
     }
 
     // 전송 버튼 클릭 이벤트
-    const sendImage = (event) => {
+    const sendImage = () => {
         try {
-            const request = axios.post(`${USER_SERVER}/getimg`, {fileImg})
-            .then(function (response) {console.log(response);})
+            const request = axios.post(`${USER_SERVER}/getimg`, {fileImg}, {
+                headers: { "Content-Type": `multipart/form-data` }
+                }
+            ).then(function (response) {console.log(response);})
             return {
                 payload: request
             }
+           
         }
         catch(e){
             console.log("error");
         }
     
     }
- 
+ //<BlankTop DesktopMargin='3' TabletMargin='3' MobileMargin='1'/>
     return(
         <div>
             <div className="nav">
-                
+                <img src={logo} alt="logo" />
+                <Link to="/" >Main </Link>
+                <Link to="/signup" >Signup </Link>
+                <Link to="/login" >Login </Link>
             </div>
-            <BlankTop DesktopMargin='3' TabletMargin='3' MobileMargin='1'/>
-            <button onClick={() => history.push((`/`))}>메인페이지</button>
-            <button onClick={() => history.push((`/signup`))}>회원가입</button>
-            <button onClick={() => history.push((`/login`))}>로그인</button>
-            <div style={border}>
-                 <img style={{"objectFit": "fill", "width": "150px", "height": "200px"}} src={fileUrl} alt={fileUrl} />
+            <div className="content">
+                <div style={divBorder}>
+                    <img style={{"objectFit": "fill", "width": "150px", "height": "200px"}} src={fileUrl} alt={fileUrl} />
+                </div>
+                <div>
+                    <input type='file'
+                        accept='image/*'
+                        name='question_img'
+                        onChange={processImage}>
+                    </input>
+                    <button onClick={sendImage}>전송</button>
+                </div>
             </div>
-            <div>
-                <input type='file'
-                    accept='image/*'
-                    name='question_img'
-                    onChange={processImage}>
-                </input>
-                <button onClick={sendImage}>전송</button>
-            </div>
-            
         </div>
     );
 }
