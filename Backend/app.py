7dcb21d3f510e427c47ea70b1da898cb90d2af96
hayoung <hayoung1214@naver.com>
2021-07-09 00:36:41 +0900

    fix : swagger response 변경

--- Backend/app.py
+++ Backend/app.py
@@ -19,15 +19,12 @@ CORS(app, supports_credentials=True)
 
 parser = reqparse.RequestParser()
 
-
-
-
-
-
-
-
-
-
+signup_parser = reqparse.RequestParser()
+login_parser = reqparse.RequestParser()
+logout_parser= reqparse.RequestParser()
+image_parser = reqparse.RequestParser()
+quiz_parser = reqparse.RequestParser()
+qmodify_parser = reqparse.RequestParser()
 
 mongo = MongoClient('localhost', 27017) # 나중에 localhost를 mongo_db 로 바꾸기
 #mongo = MongoClient('localhost', 27017)
@@ -48,14 +45,16 @@ class HelloWorld(Resource):
 
 @api.route('/signup')
 class Signup(Resource):
-    signup_parser = reqparse.RequestParser()
+
     signup_parser.add_argument('id', required=True, location='json',type=str, help='아이디')
     signup_parser.add_argument('name', required= True, location='json',type=str, help='사용자명')
     signup_parser.add_argument('password', required=True, location='json',type=str, help="비밀번호")
+
     @api.expect(signup_parser)
     @api.response(200, 'Success')
     @api.response(400, 'Bad Request')
-    @api.response(401, "아이디가 이미 있습니다.")
+    @api.response(403, "아이디가 이미 있습니다.")
+
     def post(self):
         
         new_user = request.json
@@ -64,7 +63,7 @@ class Signup(Resource):
     
         if result :  #일치하는 아이디가 있음
             return jsonify({
-                "status": 401,
+                "status": 403,
                 "success": False,
                 "message": "아이디가 이미 있습니다."
             }) 
@@ -92,14 +91,15 @@ class Signup(Resource):
 
 @api.route('/login')
 class login(Resource):
-    login_parser = reqparse.RequestParser()
+
     login_parser.add_argument('id', required=True, location='json',type=str, help='아이디')
     login_parser.add_argument('password', required=True, location='json',type=str, help="비밀번호")
+
     @api.expect(login_parser)
     @api.response(200, 'Success')
     @api.response(400, 'Bad Request')
-    @api.response(401, "해당 아이디가 없습니다")
-    @api.response(402, "비밀번호가 틀렸습니다.")
+    @api.response(403, "해당 아이디가 없습니다.\n 비밀번호가 틀렸습니다.")
+ 
     def post(self):  
         login_user = request.json
         id = login_user['id']
@@ -108,7 +108,7 @@ class login(Resource):
         result = user.find_one({ "id" : id })   #user table에서 일치하는 아이디 검색
         if result is None:  #일치하는 아이디가 없음
             return jsonify({
-                "status": 401,
+                "status": 403,
                 "success": False,
                 "message": "해당 아이디가 없습니다"
             }) 
@@ -139,7 +139,7 @@ class login(Resource):
 
         else:
             return jsonify({
-                "status": 402,
+                "status": 403,
                 "success": False,
                 "message": "비밀번호가 틀렸습니다."
             })
@@ -147,10 +147,11 @@ class login(Resource):
 
 @api.route('/logout')
 class logout(Resource):
-    logout_parser= reqparse.RequestParser()
+
     @api.expect(logout_parser)
     @api.response(200, 'Success')
     @api.response(400, 'Bad Request')
+
     def post(self):  
         session.pop('id',None)
         return jsonify({
@@ -161,12 +162,14 @@ class logout(Resource):
 
 @api.route('/quizupload')
 class Image(Resource):
-    image_parser = reqparse.RequestParser()
+    
     image_parser.add_argument('image', required=True, location='files', help="문제 이미지")
+
     @api.expect(image_parser)
     @api.response(200, 'Success')
     @api.response(400, 'Bad Request')
     @api.response(401, '로그인 필요')
+
     def post(self):
         args = image_parser.parse_args()
         id = request.cookies.get('jwt')
@@ -207,11 +210,12 @@ class Image(Resource):
 
 @api.route('/showquiz')
 class Showquiz(Resource):
-    quiz_parser = reqparse.RequestParser()
+
     @api.expect(quiz_parser)
     @api.response(200, 'Success')
     @api.response(400, 'Bad Request')
     @api.response(401, '로그인 필요')
+
     def get(self):
         
         id = request.cookies.get('jwt')
@@ -243,13 +247,14 @@ class Showquiz(Resource):
 
 @api.route('/quizmodify')
 class Quizmodify(Resource):
-    qmodify_parser = reqparse.RequestParser()
+
     qmodify_parser.add_argument('_id', required=True, location='json',type=str, help="quiz 아이디")
     qmodify_parser.add_argument('title', required=True, location='json',type=str, help="title")
     qmodify_parser.add_argument('choices', required=True, location='json',type=list, help="choices")
     qmodify_parser.add_argument('answer', required=True, location='json',type=int, help="answer")
     qmodify_parser.add_argument('script', required=True, location='json',type=str, help="script")
     qmodify_parser.add_argument('image', required=True, location='json',type=str, help="image") # 추후에 file type으로 변경 가능성 있음
+
     @api.expect(qmodify_parser)
     @api.response(200, 'Success')
     @api.response(400, 'Bad Request')
