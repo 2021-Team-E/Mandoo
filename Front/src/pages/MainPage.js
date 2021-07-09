44fa5b8831e146f23e8f8e2e25825746019a2639
shxnyoung <psh990331@gmail.com>
2021-07-09 15:18:16 +0900

    feat: table 형태 수정 및 table에  data 추가

--- Front/src/pages/MainPage.js
+++ Front/src/pages/MainPage.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import Header from '../components/Header'
+import Table from '../components/Table'
 //import BlankTop from '../components/BlankTop';
 //import {useHistory} from 'react-router-dom';
 import {useState} from 'react';
@@ -20,6 +21,74 @@ const MainPage = (props) => {
     const [fileUrl, setFileUrl] = useState(null);
     const [fileImg, setFileImg] = useState(null);
 
+    //input data
+    const columns = useMemo(
+        () => [
+            {
+                accessor: "qid",
+                Header: "문항번호",
+            },
+            {
+                accessor: "title",
+                Header: "문항내용",
+            },
+            {
+                accessor: "script" ,
+                Header: "참고내용",
+            },
+            {
+                accessor: "choice1",
+                Header: "선택01",
+            },
+            {
+                accessor: "choice2",
+                Header: "선택02",
+            },
+            {
+                accessor: "choice3",
+                Header: "선택03",
+            },
+            {
+                accessor: "choice4",
+                Header: "선택04",
+            },
+            {
+                accessor: "choice5",
+                Header: "선택05",
+            },
+            {
+                accessor: "answer",
+                Header: "정답",
+            },
+            {
+                accessor: "score",
+                Header: "점수",
+            },
+        ],
+        []
+    );
+    const data = useMemo( 
+        () =>
+        Array(31)
+            .fill()
+            .map(()=>({
+                //여기에 db랑 연결하는 코드 각각 작성
+                qid: "0001",
+                title: "다음 중 가장 적절한 것은?",
+                choice1: "1번. 여인을 슬프고 우울해 보이게 그린 것을 보니~",
+                choice2: "2번. 해바라기를 강조한 화면 구성을 보니~~",
+                choice3:"3번. 해바라기의 노란색과 윤각의 빨간색을 대비한 것을 보니~~",
+                choice4:"4번. 해바라기, 꽃병, 배경 등을 화려한 흰 색으로 그린 것을 보니~~",
+                choice5:"5번. 해바라기 꽃병과 여인을 원근법에 어긋나게 그린 것을 보니~~",
+                answer: 1,
+                script: "독일 표현주의 화가인 키르히너의 <해바라기와 여인의 얼굴>은",
+                image: "https://s3.ap-northeast-2.amazonaws.com/event-localnaeil/FileData/Article/202004/56c1b8c3-43e6-466b-8d66-6e56a770206c.jpg",
+                score: "3점",
+            })),
+        []
+    );
+
+
     // 이미지 업로드 버튼 이벤트. 미리보기 
     function processImage(event) {
         const imageFile = event.target.files[0];
@@ -35,7 +104,7 @@ const MainPage = (props) => {
         formData.append("image", fileImg)
         console.log(formData);
         try {
-            const request = axios.post(`${USER_SERVER}/quizupload`, formData, {withCredentials:true})
+            const request = axios.post(`${USER_SERVER}/imageupload`, formData, {withCredentials:true})
             .then(function (response) {
                 console.log(response);
             })
@@ -65,46 +134,8 @@ const MainPage = (props) => {
                     <button onClick={sendImage}>전송</button>
                 </div>
             </div>
-            <div className="table" align="right" style={{"float":"right", "marginRight":"100px","marginTop":"100px"}} >
-                <table border ="1"  width="1000" height="500" align="center">
-                    <thead>
-                        <tr align="center">
-                            <td width="100">문항번호</td>
-                            <td width="150">문항내용</td>
-                            <td width="100">참고내용</td>
-                            <td width="70">선택01</td>
-                            <td width="70">선택02</td>
-                            <td width="70"> 선택03</td>
-                            <td width="70">선택04</td>
-                            <td width="50">정답</td>
-                            <td width="50">점수</td>
-                        </tr>
-                    </thead>
-                    <tbody align="center">
-                        <tr>
-                            <td><input style={{"width":"100px"}} type="text" /></td>
-                            <td><input style={{"width":"150px"}} type="text" /></td>
-                            <td><input style={{"width":"100px"}} type="text" /></td>
-                            <td><input style={{"width":"70px"}} type="text" /></td>
-                            <td><input style={{"width":"70px"}} type="text" /></td>
-                            <td><input style={{"width":"70px"}} type="text" /></td>
-                            <td><input style={{"width":"70px"}} type="text" /></td>
-                            <td><input style={{"width":"50px"}} type="text" /></td>
-                            <td><input style={{"width":"50px"}} type="text" /></td>
-                        </tr>
-                        <tr>
-                        <td><input style={{"width":"100px"}} type="text" /></td>
-                            <td><input style={{"width":"150px"}} type="text" /></td>
-                            <td><input style={{"width":"100px"}} type="text" /></td>
-                            <td><input style={{"width":"70px"}} type="text" /></td>
-                            <td><input style={{"width":"70px"}} type="text" /></td>
-                            <td><input style={{"width":"70px"}} type="text" /></td>
-                            <td><input style={{"width":"70px"}} type="text" /></td>
-                            <td><input style={{"width":"50px"}} type="text" /></td>
-                            <td><input style={{"width":"50px"}} type="text" /></td>
-                        </tr>
-                    </tbody>
-                </table>
+            <div className="table" align="right" style={{"float":"right", "marginRight":"100px","marginTop":"100px","width": "1100px", "height": "600px", "overflow":"auto"}} >
+                <Table columns ={columns} data={data}/>
             </div>
             <div className="confirm" style={{"clear":"both"}}>
                     <button>확정</button>
