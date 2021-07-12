9eceb9e20f615031161a1baceb356c1e568e21f2
Yoonjae <yunyun3599@gmail.com>
2021-07-12 21:07:04 +0900

    feat: 백에서 데이터 받아와서 표에 보이게 하기

--- Front/src/components/Table.js
+++ Front/src/components/Table.js
@@ -1,19 +1,18 @@
-import React from 'react';
+import React from "react";
 import { useTable } from "react-table";
 
+function Table({ columns, data }) {
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data });
 
-function Table({columns, data}) {
-    const {
-        getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        rows,
-        prepareRow,
-      } = useTable({ columns, data });
-
-    return (
-    <table {...getTableProps()} border ="1" align="center" style={{"backgroundColor":"white", "borderCollapse":"collapse"}}>
-        <thead style={{"borderBottom":"2px solid #036", "color":"#369"}}>
+  return (
+    <table
+      {...getTableProps()}
+      border="1"
+      align="center"
+      style={{ backgroundColor: "white", borderCollapse: "collapse" }}
+    >
+      <thead style={{ borderBottom: "2px solid #036", color: "#369" }}>
         {headerGroups.map((headerGroup) => (
           <tr {...headerGroup.getHeaderGroupProps()}>
             {headerGroup.headers.map((column) => (
@@ -22,20 +21,20 @@ function Table({columns, data}) {
           </tr>
         ))}
       </thead>
-      <tbody {...getTableBodyProps()}  align="center">
+      <tbody {...getTableBodyProps()} align="center">
         {rows.map((row) => {
           prepareRow(row);
           return (
             <tr {...row.getRowProps()}>
-              {row.cells.map((cell) => (
-                <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
-              ))}
+              {row.cells.map((cell) => {
+                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+              })}
             </tr>
           );
         })}
       </tbody>
     </table>
-    );
+  );
 }
 
-export default Table
+export default Table;
