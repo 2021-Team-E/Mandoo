56972df78ce0e12b75f296bbddcf992e91db320d
shxnyoung <psh990331@gmail.com>
2021-07-13 14:08:33 +0900

    feat: delete 추가

--- Front/src/pages/MainPage.js
+++ Front/src/pages/MainPage.js
@@ -139,6 +139,30 @@ const MainPage = (props) => {
         accessor: "score",
         Header: "점수",
       },
+      {
+        Header: "Delete",
+        id: "delete",
+        accessor: (str) => "delete",
+
+        Cell: (tableProps) => (
+          <span
+            style={{
+              cursor: "pointer",
+              color: "blue",
+              textDecoration: "underline",
+            }}
+            onClick={() => {
+              // ES6 Syntax use the rvalue if your data is an array.
+              const dataCopy = [...quizzes];
+              // It should not matter what you name tableProps. It made the most sense to me.
+              dataCopy.splice(tableProps.row.index, 1);
+              setQuizzes(dataCopy);
+            }}
+          >
+            Delete
+          </span>
+        ),
+      },
     ],
     []
   );
