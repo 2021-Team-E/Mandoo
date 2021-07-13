8fce30700f3693b7900421e9a432f39beece7215
leecr1215 <leecr1215@naver.com>
2021-07-13 14:09:07 +0900

    fix: 모달창 열면 이미지 초기화

--- Front/src/components/Table.js
+++ Front/src/components/Table.js
@@ -1,18 +1,19 @@
-import React from "react";
+import React from 'react';
 import { useTable } from "react-table";
 
-function Table({ columns, data }) {
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable({ columns, data });
 
-  return (
-    <table
-      {...getTableProps()}
-      border="1"
-      align="center"
-      style={{ backgroundColor: "white", borderCollapse: "collapse" }}
-    >
-      <thead style={{ borderBottom: "2px solid #036", color: "#369" }}>
+function Table({columns, data}) {
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+      } = useTable({ columns, data });
+
+    return (
+    <table {...getTableProps()} border ="1" align="center" style={{"backgroundColor":"white", "borderCollapse":"collapse", "width":"100%"}}>
+        <thead style={{"borderBottom":"2px solid #036", "color":"#369"}}>
         {headerGroups.map((headerGroup) => (
           <tr {...headerGroup.getHeaderGroupProps()}>
             {headerGroup.headers.map((column) => (
@@ -21,20 +22,20 @@ function Table({ columns, data }) {
           </tr>
         ))}
       </thead>
-      <tbody {...getTableBodyProps()} align="center">
+      <tbody {...getTableBodyProps()}  align="center">
         {rows.map((row) => {
           prepareRow(row);
           return (
             <tr {...row.getRowProps()}>
-              {row.cells.map((cell) => {
-                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
-              })}
+              {row.cells.map((cell) => (
+                <td {...cell.getCellProps()}><input type="text" value={cell.render("Cell")}/></td>
+              ))}
             </tr>
           );
         })}
       </tbody>
     </table>
-  );
+    );
 }
 
-export default Table;
+export default Table
