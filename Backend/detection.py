309a4f056e3e9fd2a2274d6bac2f1cbf5b3a9d61
hayoung <hayoung1214@naver.com>
2021-07-14 22:34:59 +0900

    fix : tesseract 문자 추출 완료

--- Backend/detection.py
+++ Backend/detection.py
@@ -6,11 +6,11 @@ from PIL import ImageEnhance, ImageFilter, Image
 import os
 import boto3
 from s3 import BUCKET_NAME
-
+pytesseract.pytesseract.tesseract_cmd="C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
 resource = boto3.resource('s3')
 buckets = resource.Bucket(name=BUCKET_NAME)
 
-file_path = 'model_ex-024_acc-1.000000.h5'  # 내 서버에 저장하는 것 (텍스트 추출 모델)
+file_path = 'model_ex-024_acc-1.000000.h5'  # 내 서버에 저장하는 것 (텍스트 추출 모델)--이미지 판단 모델인듯
 key_name = "model_ex-024_acc-1.000000.h5"   # s3버킷 저장되어있는 이름
 buckets.download_file(key_name, file_path)
 
@@ -31,37 +31,41 @@ def get_img(image):
 
     detections = detector.detectObjectsFromImage(input_image=image_path, output_image_path=result_path , extract_detected_objects=True,  minimum_percentage_probability=80)
     
-    # for detection in detections[0]:   
-    #     print(detection)
-    #     print("\n")
+    for detection in detections:   
+        print(detection)
+        print("\n")
 
     for detection in detections[0]:  #eachObject
         print(detection["name"], " : ", detection["percentage_probability"], " : ", detection["box_points"])
-    # #텍스트 추출 부분
-    # execution_path = os.getcwd()    
-    # print(execution_path)
 
-    # prediction = CustomImageClassification()
-    # prediction.setModelTypeAsInceptionV3()
-    
-    # prediction.setModelPath('./model_ex-024_acc-1.000000.h5') #"model_ex-024_acc-1.000000.h5"
-    # prediction.setJsonPath("./model_class.json")
-    # prediction.loadModel(num_objects=2)
-    
-    # imageTrial_path = "./result/"+image #"사진주소"
-    
-    # predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, imageTrial_path), result_count=2)
-    # text = pytesseract.image_to_string(Image.open(imageTrial_path), lang='kor+eng')
+    distri_image = detections[1]
 
-    # if probabilities[0] > probabilities[1]:
-    #     print("This is a(n) " + predictions[0])
-    # else:
-    #     print("This is a(n) " + predictions[1])
-    # if predictions[0] == "text":
-    #     print(text)
-    # #The below is just to check the likelihood
-    # for eachPrediction, eachProbability in zip(predictions, probabilities):
-    #     print(eachPrediction , " : " , eachProbability)
+    #텍스트 추출 부분
+    execution_path = os.getcwd()    
+    print(execution_path)
+
+    prediction = CustomImageClassification()
+    prediction.setModelTypeAsInceptionV3()
+    
+    prediction.setModelPath('./model_ex-024_acc-1.000000.h5') #"model_ex-024_acc-1.000000.h5"
+    prediction.setJsonPath("./model_class.json")
+    prediction.loadModel(num_objects=2)
+    
+    imageTrial_path = "./result/"+image+"-objects/answer-00001.jpg" #"구분된 사진주소"
+    
+    # for detection in detections[0]:
+    predictions, probabilities = prediction.classifyImage(imageTrial_path, result_count=2)
+    text = pytesseract.image_to_string(Image.open(imageTrial_path), lang='kor+eng')
+
+    if probabilities[0] > probabilities[1]:
+        print("This is a(n) " + predictions[0])
+    else:
+        print("This is a(n) " + predictions[1])
+    if predictions[0] == "text":
+        print(text)
+    #The below is just to check the likelihood
+    for eachPrediction, eachProbability in zip(predictions, probabilities):
+        print(eachPrediction , " : " , eachProbability)
     
 
     
