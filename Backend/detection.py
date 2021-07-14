bec4aae6eeba325f2e3726d55f15ffb52545b3c6
leecr1215 <leecr1215@naver.com>
2021-07-14 21:57:40 +0900

    fix: detection.py

--- Backend/detection.py
+++ Backend/detection.py
@@ -14,27 +14,6 @@ def get_img(image):
     key_name = "model_ex-024_acc-1.000000.h5"
     buckets.download_file(key_name, file_path)
 
-<<<<<<< HEAD
-     #detector = CustomObjectDetection()
-     #detector.setModelTypeAsYOLOv3()
-     #detector.setModelPath("./detection_model-ex-025--loss-0018.418.h5") # 가중치 모델 경로
-     #detector.setJsonPath("./detection_config.json") # json 파일 경로
-     #detector.loadModel()
-
-     #image_path="./upload/"+image #input 이미지 경로
-     #result_path="./result/"+image #test output 이미지 경로 (크롭 x, 결과확인용)
-
-     #detections = detector.detectObjectsFromImage(input_image=image_path, output_image_path=result_path ,minimum_percentage_probability=80)
-     #for detection in detections:
-     #    print(detection["name"], " : ", detection["percentage_probability"], " : ", detection["box_points"])
-
-     title="test"
-     choices=[1122,2222,3232,4224,5225]
-     answer=1222
-     script="script"
-     image="image"
-     return title, choices, answer, script, image
-=======
     #detector = CustomObjectDetection()
     #detector.setModelTypeAsYOLOv3()
     #detector.setModelPath("./detection_model-ex-025--loss-0018.418.h5") # 가중치 모델 경로
@@ -80,4 +59,3 @@ def get_img(image):
     image="image"
     score = "2"
     return title, choices, answer, script, image, score
->>>>>>> b8ebafd42a2f18b783fd4695c604671b78ec8dc0
