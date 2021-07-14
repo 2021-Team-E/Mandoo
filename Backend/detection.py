f6896075248600da8006cd928ec1c2ae1c012aa4
hayoung <hayoung1214@naver.com>
2021-07-14 21:33:05 +0900

    fix : 크롭까지 완료

--- Backend/detection.py
+++ Backend/detection.py
@@ -10,36 +10,40 @@ from s3 import BUCKET_NAME
 resource = boto3.resource('s3')
 buckets = resource.Bucket(name=BUCKET_NAME)
 
-file_path = 'model_ex-024_acc-1.000000.h5'  # 이 폴더 하에 저장하는 것 (텍스트 추출 모델)
+file_path = 'model_ex-024_acc-1.000000.h5'  # 내 서버에 저장하는 것 (텍스트 추출 모델)
 key_name = "model_ex-024_acc-1.000000.h5"   # s3버킷 저장되어있는 이름
 buckets.download_file(key_name, file_path)
 
-file_path = 'detection_config.json'  # 이 폴더 하에 저장하는 것 (텍스트 추출 모델)
-key_name = 'detection_config.json'   # s3버킷 저장되어있는 이름
+file_path = 'detection_config.json'  
+key_name = 'detection_config.json'  
 buckets.download_file(key_name, file_path)
 
 def get_img(image):
     
-    # detector = CustomObjectDetection()
-    # detector.setModelTypeAsYOLOv3()
-    # detector.setModelPath("./detection_model-ex-025--loss-0018.418.h5") # 가중치 모델 경로 (3구분 모델)
-    # detector.setJsonPath("./detection_config.json") # json 파일 경로
-    # detector.loadModel()
+    detector = CustomObjectDetection()
+    detector.setModelTypeAsYOLOv3()
+    detector.setModelPath("./detection_model-ex-025--loss-0018.418-85per.h5") # 가중치 모델 경로 (3구분 모델)
+    detector.setJsonPath("./detection_config.json") # json 파일 경로
+    detector.loadModel()
 
-    # image_path="./upload/"+image #input 이미지 경로
-    # result_path="./result/"+image #test output 이미지 경로 (크롭 x, 결과확인용)
+    image_path="./upload/"+image #input 이미지 경로
+    result_path="./result/"+image #test output 이미지 경로 (크롭 x, 결과확인용)
 
-    # detections = detector.detectObjectsFromImage(input_image=image_path, output_image_path=result_path , extract_detected_objects=True,  minimum_percentage_probability=80)
-    # for detection in detections:   #eachObject
+    detections = detector.detectObjectsFromImage(input_image=image_path, output_image_path=result_path , extract_detected_objects=True,  minimum_percentage_probability=80)
+    
+    # for detection in detections:    #eachObject
     #     print(detection["name"], " : ", detection["percentage_probability"], " : ", detection["box_points"])
 
-    # execution_path = os.getcwd()    # 
-    
+
+    # #텍스트 추출 부분
+    # execution_path = os.getcwd()    
+    # print(execution_path)
+
     # prediction = CustomImageClassification()
     # prediction.setModelTypeAsInceptionV3()
     
-    # prediction.setModelPath(os.path.join(execution_path,"가중치 모델")) #"model_ex-024_acc-1.000000.h5"
-    # prediction.setJsonPath(os.path.join(execution_path, "./model_class.json"))
+    # prediction.setModelPath('./model_ex-024_acc-1.000000.h5') #"model_ex-024_acc-1.000000.h5"
+    # prediction.setJsonPath("./model_class.json")
     # prediction.loadModel(num_objects=2)
     # imageTrial_path = "사진주소"
     
