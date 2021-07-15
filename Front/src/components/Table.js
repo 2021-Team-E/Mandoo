f677c68bdea3e3b7e74a150dbc809b7ee5fda77f
shxnyoung <psh990331@gmail.com>
2021-07-15 17:07:00 +0900

    feat:

--- Front/src/components/Table.js
+++ Front/src/components/Table.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { useTable, useGlobalFilter, useSortBy } from "react-table";
-import Search from "./Search";
+import { useTable } from "react-table";
 
 function Table({ columns, data }) {
   const {
@@ -13,45 +12,48 @@ function Table({ columns, data }) {
   } = useTable({ columns, data }, useGlobalFilter, useSortBy);
 
   return (
-    <>
-      <Search onSubmit={setGlobalFilter} />{" "}
-      {/*submit시 row 배열에 필터링된 검색결과 반영됨 */}
-      <table
-        {...getTableProps()}
-        border="1"
-        align="center"
-        style={{
-          backgroundColor: "white",
-          borderCollapse: "collapse",
-          width: "100%",
-        }}
-      >
-        <thead style={{ borderBottom: "2px solid #036", color: "#369" }}>
-          {headerGroups.map((headerGroup) => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
-                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                  {/*테이블 헤더 한번 클릭하면 데이터 오름차순 정렬, 다시 클릭하면 내림차순 정렬됨 */}
-                  {column.render("Header")}
-                </th>
+    <table
+      {...getTableProps()}
+      border="1"
+      align="center"
+      style={{
+        backgroundColor: "white",
+        borderCollapse: "collapse",
+        width: "100%",
+      }}
+    >
+      <thead style={{ borderBottom: "2px solid #036", color: "white" }}>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th
+                {...column.getHeaderProps()}
+                style={{
+                  top: "0",
+                  position: "sticky",
+                  backgroundColor: "#369",
+                }}
+              >
+                {column.render("Header")}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+
+      <tbody {...getTableBodyProps()} align="center">
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => (
+                <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
               ))}
             </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()} align="center">
-          {rows.map((row) => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map((cell) => (
-                  <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
-                ))}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </>
+          );
+        })}
+      </tbody>
+    </table>
   );
 }
 
