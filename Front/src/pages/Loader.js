de7718e8b817078699e8caab0d3cdfa0b341bad1
shxnyoung <psh990331@gmail.com>
2021-07-16 16:06:23 +0900

    fix: 머지 오류

---
+++ Front/src/pages/Loader.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactLoading from 'react-loading';
+
+function Loader({ type, color, message }) {
+  return (
+    <div class="contentWrap">
+      <div
+        style={{
+          position: 'fixed',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+        }}
+      >
+        <h2>{message}</h2>
+        <h2>잠시만 기다려주세요.</h2>
+        <br />
+        <br />
+        <ReactLoading type={type} color={color} height={'80%'} width={'80%'} />
+      </div>
+    </div>
+  );
+}
+
+export default Loader;
