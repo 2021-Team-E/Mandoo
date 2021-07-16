d5c873b1257a16dd52aea1cf9df5671e7023760b
leecr1215 <leecr1215@naver.com>
2021-07-16 15:44:00 +0900

    fix: 로딩창 text 수정

--- Front/src/components/Modals/Modal.js
+++ Front/src/components/Modals/Modal.js
@@ -1,33 +1,27 @@
-import React, { ReactElement } from 'react';
-//import Modal from "react-modal";
-//import styled, { keyframes } from "styled-components";
-import "./Modal.css";
-
-//Modal.setAppElement("#root");
-
-
+import React from 'react';
+import './Modal.css';
 const Modal = (props) => {
-    const { open, close } = props;
+  const { open, close } = props;
 
-    return (
-        <div className={ open ? 'openModal modal':'modal'}>
-            { open ? (
-                <section>
-                    <header>
-                        <h3>이미지 등록하기</h3>
-                        <button className="close" onClick={close}>&times;</button>
-                    </header>
-                    <main>
-                        {props.children}
-                    </main>
-                    <footer>
-                        <button className="close" onClick={close}>
-                            close
-                        </button>
-                    </footer>                       
-                </section>
-            ): null} 
-        </div>
-    );
-}
-export default Modal
+  return (
+    <div className={open ? 'openModal modal' : 'modal'}>
+      {open ? (
+        <section>
+          <header>
+            <h3>이미지 등록하기</h3>
+            <button className="close" onClick={close}>
+              &times;
+            </button>
+          </header>
+          <main>{props.children}</main>
+          <footer>
+            <button className="close" onClick={close}>
+              close
+            </button>
+          </footer>
+        </section>
+      ) : null}
+    </div>
+  );
+};
+export default Modal;
