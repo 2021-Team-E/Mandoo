43e10568e19d83f6434e0e9f1f3bf0142ae4d0b6
hayoung <hayoung1214@naver.com>
2021-07-20 17:30:31 +0900

    feat : yolov5 pytesseract 기능 추가

--- Backend/detection.py
+++ Backend/detection.py
@@ -1,6 +1,3 @@
-
-#from imageai.Detection.Custom import CustomObjectDetection
-#from imageai.Classification.Custom import CustomImageClassification
 import pytesseract 
 from PIL import Image
 import os
@@ -14,7 +11,6 @@ from pathlib import Path
 
 import cv2
 import torch
-from numpy import random
 
 from models.experimental import attempt_load
 from utils.datasets import LoadImages
@@ -27,7 +23,6 @@ pytesseract.pytesseract.tesseract_cmd="C:\\Program Files\\Tesseract-OCR\\tessera
 s3 = boto3.client('s3', aws_access_key_id = AWS_ACCESS_KEY, aws_secret_access_key = AWS_SECRET_KEY)
 resource = boto3.resource('s3', aws_access_key_id = AWS_ACCESS_KEY, aws_secret_access_key = AWS_SECRET_KEY)
 buckets = resource.Bucket(name=BUCKET_NAME)
-
 
 ##모델 로드 부분
 weights = 'modelv2.0.pt'
@@ -75,8 +70,8 @@ def get_img(image):
 
     # Get names and colors
     names = model.module.names if hasattr(model, 'module') else model.names
-    print(names)
-   
+    labels= []
+    dict = {"question": [], "content" :[], "answer" : []}
 
     # Run inference
     if device.type != 'cpu':
@@ -96,7 +91,7 @@ def get_img(image):
 
         # Process detections
         for i, det in enumerate(pred):  # detections per image
-            # det = det[det[:, -1] == 1]
+            print(i)
             p, s, im0, frame = Path(path), '', im0s, getattr(dataset, 'frame', 0)
 
             save_path = str(save_dir / p.name)
@@ -115,17 +110,49 @@ def get_img(image):
                 # Write results
                 for *xyxy, conf, cls in reversed(det):
                     c = int(cls)  # integer class
-                    print(c)
+                    
                     label =  f'{names[c]} {conf:.2f}'
-                    print(label)
-                    #label = f'{names[int(cls)]} {conf:.2f}'
+
+                    labels.append(names[c])
+                    print(labels)
+                
                     plot_one_box(xyxy, im0, label=label, color=colors(c, True), line_thickness=3)
                     if save_crop:
-                            save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
+                            crop_path = save_dir / 'crops' / names[c] / f'{p.stem}.jpg'
+                            save_one_box(xyxy, imc, file=crop_path, BGR=True)
 
-                result = "True"
-            else:
-                result = "False"
+                    if names[c] == "content" : # content로 판별시 이미지 여부 판단 모델 들어가지 않고 이미지 자체를 return 데이터에 담음
+                        imagefilename ="result/"+ image + "_" + names[c] + "_"+ str(i) + "_" + str(datetime.datetime.now()).replace("\\","/").replace(" ","").replace(":","")+".jpeg"
+                        imagefilename.replace(" ","")
+                        imagetoupload = open( crop_path , "rb")
+                        s3.put_object(Body=imagetoupload, Bucket=BUCKET_NAME, Key=imagefilename, ContentType="image/jpeg")
+                        img_url = "https://summer-program.s3.ap-northeast-2.amazonaws.com/"+imagefilename
+                        dict[names[c]].append(img_url)
+                        continue
+                    
+                    # 이미지/text 감별 모델 부분
+                    # predictions, probabilities = prediction.classifyImage(detection, result_count=2)    #predictions[0] : 무조건 퍼센트 높은 아이로 지정됨
+                        
+                        
+                        
+                    # if probabilities[0] > probabilities[1]:
+                    #     print("This is a(n) " + predictions[0])
+
+                    # if predictions[0] == "text": #텍스트인 경우
+                    text = pytesseract.image_to_string(Image.open(crop_path), lang='kor+eng')
+                    print(text)
+                    dict[names[c]].append(text)
+
+                    # else :  #이미지 포함한 경우
+                    #     imagefilename ="result/"+ image + "_" + this_labelname + "_"+ str(index) + "_" + str(datetime.datetime.now())+".jpeg"
+                    #     imagefilename.replace(" ","")
+                    #     imagetoupload = open(det, "rb")
+                    #     s3.put_object(Body=imagetoupload, Bucket=BUCKET_NAME, Key=imagefilename, ContentType="image/jpeg")
+                    #     img_url = "https://summer-program.s3.ap-northeast-2.amazonaws.com/"+imagefilename
+                    #     dict[this_labelname].append(img_url)
+                    # print("\n\n")
+
+
 
             # Save results (image with detections)
             if save_img:
@@ -133,97 +160,15 @@ def get_img(image):
                     cv2.imwrite(save_path, im0)
                     print(save_path)
 
-    # return result
+       
 
-    # detector = CustomObjectDetection()
-    # detector.setModelTypeAsYOLOv3()
-    # detector.setModelPath("./detection_model-ex-025--loss-0018.418-85per.h5") # 가중치 모델 경로 (3구분 모델)
-    # detector.setJsonPath("./detection_config.json") # json 파일 경로
-    # detector.loadModel()
-
-    # image_path="./upload/"+image_png #input 이미지 경로
-    # result_path="./result/"+image_png #test output 이미지 경로 (크롭 x, 결과확인용)
-
-    # detections = detector.detectObjectsFromImage(input_image=image_path, output_image_path=result_path , extract_detected_objects=True,  minimum_percentage_probability=50)
-    
-    # label= []
-    # dict = {"question": [], "content" :[], "answer" : []}
-   
-
-    # for detection in detections:   
-    #     #print(detection)
-    #     print("\n")
-
-    # for detection in detections[0]:
-    #     print(detection["name"], " : ", detection["percentage_probability"], " : ", detection["box_points"])
-    #     label.append(detection["name"])
-    # print(label)
-    
-
-    # #텍스트 추출 부분
-    # execution_path = os.getcwd()    
-    # print(execution_path)
-
-    # prediction = CustomImageClassification()
-    # prediction.setModelTypeAsInceptionV3()
-    
-    # prediction.setModelPath('./model_ex-024_acc-1.000000.h5')
-    # prediction.setJsonPath("./model_class.json")
-    # prediction.loadModel(num_objects=2)
-    
-    
-    
-    # for index,detection in enumerate(detections[1]): #index로 몇번째 인지 접근 가능
-    #     print(index, detection)
-    #     this_labelname=label[index]
-    #     print(this_labelname)
-        
-    #     if this_labelname == "content" : # content로 판별시 이미지 여부 판단 모델 들어가지 않고 이미지 자체를 return 데이터에 담음
-    #         imagefilename ="result/"+ image + "_" + this_labelname + "_"+ str(index) + "_" + str(datetime.datetime.now()).replace("\\","/").replace(" ","").replace(":","")+".jpeg"
-    #         imagefilename.replace(" ","")
-    #         imagetoupload = open(detection, "rb")
-    #         s3.put_object(Body=imagetoupload, Bucket=BUCKET_NAME, Key=imagefilename, ContentType="image/jpeg")
-    #         img_url = "https://summer-program.s3.ap-northeast-2.amazonaws.com/"+imagefilename
-    #         dict[this_labelname].append(img_url)
-    #         continue
-
-    #     predictions, probabilities = prediction.classifyImage(detection, result_count=2)    #predictions[0] : 무조건 퍼센트 높은 아이로 지정됨
-        
-        
-        
-    #     if probabilities[0] > probabilities[1]:
-    #         print("This is a(n) " + predictions[0])
-
-    #     if predictions[0] == "text": #텍스트인 경우
-    #         text = pytesseract.image_to_string(Image.open(detection), lang='kor+eng')
-    #         print(text)
-    #         dict[this_labelname].append(text)
-
-    #     else :  #이미지 포함한 경우
-    #         imagefilename ="result/"+ image + "_" + this_labelname + "_"+ str(index) + "_" + str(datetime.datetime.now())+".jpeg"
-    #         imagefilename.replace(" ","")
-    #         imagetoupload = open(detection, "rb")
-    #         s3.put_object(Body=imagetoupload, Bucket=BUCKET_NAME, Key=imagefilename, ContentType="image/jpeg")
-    #         img_url = "https://summer-program.s3.ap-northeast-2.amazonaws.com/"+imagefilename
-    #         dict[this_labelname].append(img_url)
-    #     print("\n\n")
-
-    #     #The below is just to check the likelihood
-    #     for eachPrediction, eachProbability in zip(predictions, probabilities):
-    #         print(eachPrediction , " : " , eachProbability)
-
-    # #shutil.rmtree('./result/') # 결과 확인 필요 없을 때 주석 풀고 써주기 (result/ 폴더 삭제해주는 기능)
-    # print(dict)
-    # title=dict["question"]
-    # choices=dict["answer"]
-    # answer="1"
-    # script=dict["content"]
-    # score = "2"
-
-    title=["example question"]
-    choices=["example answer"]
+    #shutil.rmtree('./result/') # 결과 확인 필요 없을 때 주석 풀고 써주기 (result/ 폴더 삭제해주는 기능)
+    print(dict)
+    title=dict["question"]
+    choices=dict["answer"]
     answer="1"
-    script=["example content"]
-    score = "2"
-    
+    script=dict["content"]
+    score = "2"       
+
     return title, choices, answer, script, score
+    
