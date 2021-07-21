bb83c3f0576d8a0e71a69728f62761f4e6e6256a
hayoung <hayoung1214@naver.com>
2021-07-21 15:07:56 +0900

    fix : 각 폴더별로 result 결과 하나씩만 저장되도록 수정

--- Backend/detection.py
+++ Backend/detection.py
@@ -25,7 +25,8 @@ resource = boto3.resource('s3', aws_access_key_id = AWS_ACCESS_KEY, aws_secret_a
 buckets = resource.Bucket(name=BUCKET_NAME)
 
 ##모델 로드 부분
-weights = 'modelv2.0.pt'
+weights1 = 'modelv2.0.pt'
+weights2 = 'choice5_bestweight'
 ## s3 버킷에 weights 모델 올리면 사용
 # file_path = 'modelv1.0.pt'  # 내 서버에 저장하는 것 이미지 포함 여부 판단 모델
 # key_name = 'modelv1.0.pt'   # s3버킷 저장되어있는 이름
@@ -58,10 +59,13 @@ def get_img(image):
     half = device.type != 'cpu'  # half precision only supported on CUDA
 
     # Load model
-    model = attempt_load(weights, map_location=device)  # load FP32 model
-    imgsz = check_img_size(imgsz, s=model.stride.max())  # check img_size
+    model1 = attempt_load(weights1, map_location=device)  # load FP32 model
+    model2 = attempt_load(weights2, map_location=device)  # load FP32 model
+
+    imgsz = check_img_size(imgsz, s=model1.stride.max())  # check img_size
     if half:
-        model.half()  # to FP16
+        model1.half()  # to FP16
+        model2.half()
 
     # Set Dataloader
     save_img = True
@@ -69,13 +73,13 @@ def get_img(image):
     dataset = LoadImages(source, img_size=imgsz)
 
     # Get names and colors
-    names = model.module.names if hasattr(model, 'module') else model.names
+    names = model1.module.names if hasattr(model1, 'module') else model1.names
     labels= []
     dict = {"question": [], "content" :[], "answer" : []}
 
     # Run inference
     if device.type != 'cpu':
-        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
+        model1(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model1.parameters())))  # run once
     for path, img, im0s, vid_cap in dataset:
         img = torch.from_numpy(img).to(device)
         img = img.half() if half else img.float()  # uint8 to fp16/32
@@ -84,7 +88,7 @@ def get_img(image):
             img = img.unsqueeze(0)
 
         # Inference
-        pred = model(img, augment=False)[0]
+        pred = model1(img, augment=False)[0]
 
         # Apply NMS
         pred = non_max_suppression(pred, 0.5, 0.45, classes=None, agnostic=False)
@@ -120,6 +124,8 @@ def get_img(image):
                     if save_crop:
                             crop_path = save_dir / 'crops' / names[c] / f'{p.stem}.jpg'
                             save_one_box(xyxy, imc, file=crop_path, BGR=True)
+                            print(crop_path)
+                            print({p.stem})
 
                     if names[c] == "content" : # content로 판별시 이미지 여부 판단 모델 들어가지 않고 이미지 자체를 return 데이터에 담음
                         imagefilename ="result/"+ image + "_" + names[c] + "_"+ str(i) + "_" + str(datetime.datetime.now()).replace("\\","/").replace(" ","").replace(":","")+".jpeg"
@@ -128,9 +134,82 @@ def get_img(image):
                         s3.put_object(Body=imagetoupload, Bucket=BUCKET_NAME, Key=imagefilename, ContentType="image/jpeg")
                         img_url = "https://summer-program.s3.ap-northeast-2.amazonaws.com/"+imagefilename
                         dict[names[c]].append(img_url)
+
+                        
                         continue
                     
-                    # 이미지/text 감별 모델 부분
+                    # elif names[c] == "question" : 
+                    #     # 이미지/text 감별 모델 부분 (model3)
+                    #     # predictions, probabilities = prediction.classifyImage(detection, result_count=2)    #predictions[0] : 무조건 퍼센트 높은 아이로 지정됨
+                            
+                            
+                            
+                    #     # if probabilities[0] > probabilities[1]:
+                    #     #     print("This is a(n) " + predictions[0])
+
+                    #     # if predictions[0] == "text": #텍스트인 경우
+                    #     text = pytesseract.image_to_string(Image.open(crop_path), lang='kor+eng')
+                    #     print(text)
+                    #     dict[names[c]].append(text)
+
+                    #     # else :  #이미지 포함한 경우
+                    #     #     imagefilename ="result/"+ image + "_" + this_labelname + "_"+ str(index) + "_" + str(datetime.datetime.now())+".jpeg"
+                    #     #     imagefilename.replace(" ","")
+                    #     #     imagetoupload = open(det, "rb")
+                    #     #     s3.put_object(Body=imagetoupload, Bucket=BUCKET_NAME, Key=imagefilename, ContentType="image/jpeg")
+                    #     #     img_url = "https://summer-program.s3.ap-northeast-2.amazonaws.com/"+imagefilename
+                    #     #     dict[this_labelname].append(img_url)
+                    #     # print("\n\n")
+
+                    # elif names[c] == "answer" :
+                    #     source = crop_path  #input 이미지 경로
+                    #     dataset_answer=LoadImages(source, img_size=imgsz)
+                    #     for path, img, im0s, vid_cap in dataset_answer:
+                    #         img = torch.from_numpy(img).to(device)
+                    #         img = img.half() if half else img.float()  # uint8 to fp16/32
+                    #         img /= 255.0  # 0 - 255 to 0.0 - 1.0
+                    #         if img.ndimension() == 3:
+                    #             img = img.unsqueeze(0)
+
+                    #         # Inference
+                    #         pred = model2(img, augment=False)[0]
+
+                    #         # Apply NMS
+                    #         pred = non_max_suppression(pred, 0.5, 0.45, classes=None, agnostic=False)
+
+                    #         # Process detections
+                    #         for i, det in enumerate(pred):  # detections per image
+                    #             print(i)
+                    #             p, s, im0, frame = Path(path), '', im0s, getattr(dataset, 'frame', 0)
+
+                    #             save_path = str(save_dir / p.name)
+                    #             s += '%gx%g ' % img.shape[2:]  # print string
+                    #             gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+                    #             imc = im0.copy() if save_crop else im0  # for save_crop
+                    #             if len(det):
+                    #                 # Rescale boxes from img_size to im0 size
+                    #                 det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+                    #                 # Print results
+                    #                 for c in det[:, -1].unique():
+                    #                     n = (det[:, -1] == c).sum()  # detections per class
+                    #                     s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                    #                 # Write results
+                    #                 for *xyxy, conf, cls in reversed(det):
+                    #                     c = int(cls)  # integer class
+                                        
+                    #                     label =  f'{names[c]} {conf:.2f}'
+
+                    #                     labels.append(names[c])
+                    #                     print(labels)
+                                    
+                    #                     plot_one_box(xyxy, im0, label=label, color=colors(c, True), line_thickness=3)
+                    #                     if save_crop:
+                    #                             crop_path = save_dir / 'crops' /'answer'/ names[c] / f'{p.stem}.jpg'
+                    #                             save_one_box(xyxy, imc, file=crop_path, BGR=True)
+
+                    # 이미지/text 감별 모델 부분 (model3)
                     # predictions, probabilities = prediction.classifyImage(detection, result_count=2)    #predictions[0] : 무조건 퍼센트 높은 아이로 지정됨
                         
                         
@@ -151,6 +230,7 @@ def get_img(image):
                     #     img_url = "https://summer-program.s3.ap-northeast-2.amazonaws.com/"+imagefilename
                     #     dict[this_labelname].append(img_url)
                     # print("\n\n")
+
 
 
 
