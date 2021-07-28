6653626ea0a8874e187f31907bbfc82e63fc294d
Yoonjae <yunyun3599@gmail.com>
2021-07-28 14:37:10 +0900

    feat: 로딩창 색상 변경

--- frontend/src/pages/Loader.js
+++ frontend/src/pages/Loader.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import ReactLoading from 'react-loading';
+import React from "react";
+import ReactLoading from "react-loading";
 
 function Loader({ type, color, message }) {
   return (
     <div className="contentWrap">
       <div
         style={{
-          position: 'fixed',
-          top: '50%',
-          left: '50%',
-          transform: 'translate(-50%, -50%)',
-          marginLeft: 'auto',
-          marginRight: 'auto',
+          position: "fixed",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          marginLeft: "auto",
+          marginRight: "auto",
         }}
       >
         <h2>{message}</h2>
         <h2>잠시만 기다려주세요.</h2>
         <br />
         <br />
-        <ReactLoading type={type} color={color} height={'80%'} width={'80%'} />
+        <ReactLoading
+          type={type}
+          color="#ffebe8"
+          height={"80%"}
+          width={"80%"}
+        />
       </div>
     </div>
   );
